bae7d789e6bb3629be8cd3ba226d5874e4eb53c6
toshderv <toshderv@gmail.com>
2018-07-11 20:10:32 +0300

    Циклы и функции

--- index.php
+++ index.php
@@ -7,7 +7,51 @@
   <title>Document</title>
 </head>
 <body>
-  <?echo 'Hello World!'?>
-  <?echo 'Привет!'?>
+  <?
+    $a = 1;
+    $array = [1, 2, 'name' => 'Vadim', 4, 5, 6];
+
+    for ($i=0; $i < count($array); $i++) { 
+      echo $array[$i].'<br>';
+    }
+
+    foreach ($array as $key => $value) {
+      echo '!<br>';
+      if ($key === 0) {
+        echo 'Key: '.$key.', value: '.$value.'<br>';
+        break;
+      }
+    }
+
+    $i = 0;
+    while ($i < 10) {
+      echo $i.'<br>';
+      $i++;
+    }
+
+    $i = 0;
+    do {
+      echo $i.'<br>';
+      ++$i;
+    } while ($i < 10);
+
+    $num = 5;
+    $degree = 2;
+
+    echo myPow($num, $degree);
+
+    /**
+     * Функция для возведения числа в степень
+     * 
+     * @param int $num
+     * @param string|NULL
+     * @return int|string
+     */
+    function myPow($num, $degree) {
+      $result = $num ** $degree;
+      return $result;
+    }
+
+  ?>
 </body>
 </html>
