5f038721824dff0800d236c01c3e115721250264
toshderv <toshderv@gmail.com>
2018-07-18 20:07:00 +0300

    ScoolWork_18_07_18 edit_2

--- ScoolWork_18_07_18/task.php
+++ ScoolWork_18_07_18/task.php
@@ -2,14 +2,13 @@
 $array = ['Vadim', 'Max'];
 
 array_walk($array, function (&$value) {
-  $value = 'Hello '.$value;
+  $value = 'Hello ' . $value;
 });
 
 var_dump($array);
 
 $arrayInt = [1, 5, 7, 8, -9, 3];
 $resultNew = array_filter($arrayInt, function ($value) {
-  // var_dump($item);
   if ($value > 3 && ($value % 2)) {
     return true;
   }
